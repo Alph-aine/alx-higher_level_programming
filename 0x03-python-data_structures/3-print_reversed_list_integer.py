d25faf594ef3c55333eb269dd5268f5b3afe5bbf
Abdulbasit <aabdulrafiu721@gmail.com>
2023-02-11 00:26:58 -0800

    Added validation for list is None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):	
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return None
     length = len(my_list)
     if length == 0:
-        return None	
+        return None
     i = length - 1
     while i >= 0:
         print("{:d}".format(my_list[i]), end='\n')
