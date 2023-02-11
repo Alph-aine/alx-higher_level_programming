52feaa881af063680653a64e61589bf9908ed8be
Abdulbasit <aabdulrafiu721@gmail.com>
2023-02-11 00:22:54 -0800

    Added validation for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=[]):	
     length = len(my_list)
+    if length == 0:
+        return None	
     i = length - 1
     while i >= 0:
         print("{:d}".format(my_list[i]), end='\n')
