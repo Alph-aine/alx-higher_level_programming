4560faa155ec60b91e55991483a13298030e7dda
Abdulbasit <aabdulrafiu721@gmail.com>
2023-02-12 06:02:35 -0800

    error fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence[0] is None:
-        return
-    length = len(sentence)
-    return(length, sentence[0])
+    if len(sentence) == 0:
+        return(0, None)
+    else:
+        length = len(sentence)
+        return(length, sentence[0])
