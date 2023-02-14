515cf13466d2287ec0e6e16e53c5c0fa0940ad3d
Abdulbasit <aabdulrafiu721@gmail.com>
2023-02-14 23:33:51 +0000

    Finally ...

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result:", result)
+        print("Inside result: {}".format(result))
