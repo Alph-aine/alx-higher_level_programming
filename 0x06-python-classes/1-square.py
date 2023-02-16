030a7cd15c489548dbbca7cb2cbc33738b57d7f6
Abdulbasit <aabdulrafiu721@gmail.com>
2023-02-16 16:41:11 +0000

    using __init__

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Create a  class"""
+
+class Square:
+    """ This class has a private instance"""
+    def __init__(self, size):
+        """ this is a initializing function"""
+        self.__size = size
