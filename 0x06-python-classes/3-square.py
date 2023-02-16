660c5d749a647b1ccc08a2788c275e1c6c1b7049
Abdulbasit <aabdulrafiu721@gmail.com>
2023-02-16 17:06:17 +0000

    I am getting the hang of it

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create a  class"""
+
+
+class Square:
+    """ This class has a private instance"""
+    def __init__(self, size=0):
+        """ This is a initializing function.
+        It checks if the size passed is an integer and also > 0
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Returns the area of the current square """
+        return (self.__size ** 2)
