3d2cf5208953c3d5a6abed748dff827cc97e4015
Abdulbasit <aabdulrafiu721@gmail.com>
2023-02-17 08:34:11 -0800

    printing the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Create a  class"""
+
+
+class Square:
+    """ This class has a private instance"""
+    def __init__(self, size=0):
+        """ This is a initializing function.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ A getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A setter"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Returns the area of the current square """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """This method prints the square"""
+        i = 0
+        while (i in range(self.__size)):
+            if self.__size <= 0:
+                print()
+            else:
+                j = 0
+                while (j in range(self.__size)):
+                    print("#", end='')
+                    j += 1
+            print()
+            i += 1
