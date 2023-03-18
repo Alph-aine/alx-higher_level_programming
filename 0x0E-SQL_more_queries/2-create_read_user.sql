f9ca8988d11a7016aff1afec7efb4b64de718f35
Abdulbasit <aabdulrafiu721@gmail.com>
2023-03-18 15:29:56 +0000

    create database and user, should not fail if exists

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,3 @@
+-- create a database, then add a user. if database/user already exists, it should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
