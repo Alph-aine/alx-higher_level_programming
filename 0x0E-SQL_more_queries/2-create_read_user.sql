dbc11f15ecd08ba597f5342aa392b6d45d25a393
Abdulbasit <aabdulrafiu721@gmail.com>
2023-03-18 15:48:37 +0000

    create a database then a user, no errors if it exists

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,5 @@
 -- create a database, then add a user. if database/user already exists, it should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
