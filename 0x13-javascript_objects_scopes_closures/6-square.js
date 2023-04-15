b3c95bf7bffc1776c14fb12e04f1fa1903afad4a
Abdulbasit <aabdulrafiu721@gmail.com>
2023-04-15 19:46:07 +0100

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const square = require('./5-square.js');
+
+class Square extends square {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;
