dad60690ca8dfd204083728bf9c901b724dcaf8f
Abdulbasit <aabdulrafiu721@gmail.com>
2023-04-18 15:52:54 +0000

    error fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,18 +10,21 @@ if __name__ == '__main__':
         dbname = sys.argv[3]
         state_name = sys.argv[4]
 
-        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                               passwd=password, db=dbname)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states "
-                    "WHERE name = '{}' "
-                    "ORDER BY id ASC".format(state_name))
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
+        if state_name[0].isupper():
+            conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                                   passwd=password, db=dbname)
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM states "
+                        "WHERE name = '{}' "
+                        "ORDER BY id ASC".format(state_name))
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
 
-        cur.close()
-        conn.close()
+            cur.close()
+            conn.close()
+        else:
+            print("state_name should start with an uppercase")
 
     else:
         print("Usage: {} username password dbname state_name"
