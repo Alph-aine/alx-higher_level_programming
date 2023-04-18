daff0f5142d24a08fe13ded1557003ca47ebb3e9
Abdulbasit <aabdulrafiu721@gmail.com>
2023-04-18 11:44:15 +0000

    getting data from 2 tables with join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+''' Selects from 2 tables in a database'''
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password, db=dbname)
+
+        cur = conn.cursor()
+        query = ("SELECT cities.id, cities.name, states.name FROM cities"
+                 " INNER JOIN states on cities.state_id = states.id")
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+    else:
+        print("Usage: {} username password dbname".format(sys.argv[0]))
