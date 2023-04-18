bf164bcee674254a2d696df5545a76b21a483448
Abdulbasit <aabdulrafiu721@gmail.com>
2023-04-18 12:03:51 +0000

    subquery with user input

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' using subqueries'''
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+        state_name = sys.argv[4]
+
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password, db=dbname)
+
+        cur = conn.cursor()
+        query = ("SELECT cities.name FROM cities"
+                 " WHERE state_id = "
+                 "(SELECT id FROM states WHERE name = %s)")
+        cur.execute(query, (state_name, ))
+        rows = cur.fetchall()
+        city = []
+        for row in rows:
+            city.append(row[0])
+        print(', '.join(city))
+
+    else:
+        print("Usage: {} username password dbname state name"
+              .format(sys.argv[0]))
