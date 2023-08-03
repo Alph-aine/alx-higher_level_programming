0b28f97e5fcf75844476c57645037c1757f47581
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-03 22:54:55 +0100

    reading data from api

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (error, response, body) => {
   if (error) console.error(error);
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
